65f02ef13d1985901e7b99ef224a71bb89be0688
Carlos A. Junior <carlos.spohr@gmail.com>
2013-05-27 13:41:54 -0300

    added disabled and readOnly tags

---
+++ src/br/inf/carlos/taglibs/html/ReadOnlyTag.java
@@ -0,0 +1,43 @@
+package br.inf.carlos.taglibs.html;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+/**
+ * 
+ * @author Carlos Alberto Junior Spohr Poletto (carlosjrcabello@gmail.com)
+ */
+public class ReadOnlyTag extends TagSupport {
+
+	private static final long serialVersionUID = 1669960932711639142L;
+
+	protected Boolean test;
+	
+	@Override
+	public int doEndTag() throws JspException {
+		
+		try {
+			String readOnly = "";
+			
+			if(this.test != null && this.test.equals(Boolean.TRUE)){
+				readOnly = " readOnly='readOnly' ";
+			}
+			
+			this.pageContext.getOut().write(readOnly);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return super.doEndTag();
+	}
+
+	public Boolean getTest() {
+		return test;
+	}
+
+	public void setTest(Boolean test) {
+		this.test = test;
+	}
+}
